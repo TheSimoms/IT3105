26fdd4a4952762177b37a42ea53c35d334e1ed10
Simon B-J <simon_bj@live.com>
2015-10-20 14:14:44 +0200

    Added comments

--- src/modules/module_four/game_board.py
+++ src/modules/module_four/game_board.py
@@ -2,6 +2,7 @@ from numpy import random, add
 from copy import deepcopy
 
 
+# Dictionary containing info of the four possible moves
 DIRECTIONS = {
     'up': [[0, -1], [[0, 1, 2, 3], [0, 1, 2, 3]]],
     'right': [[1, 0], [[3, 2, 1, 0], [0, 1, 2, 3]]],
@@ -9,6 +10,7 @@ DIRECTIONS = {
     'left': [[-1, 0], [[0, 1, 2, 3], [0, 1, 2, 3]]]
 }
 
+# Indices of the cells along the path
 PATH = [
     [0, 0], [0, 1], [0, 2], [0, 3],
     [1, 3], [1, 2], [1, 1], [1, 0],
@@ -16,18 +18,22 @@ PATH = [
     [3, 3], [3, 2], [3, 1], [3, 0]
 ]
 
+# Constant to use in evaluating function
 R = 0.25
 
 
 class GameBoard:
     def __init__(self, state=None):
+        # Sets state or generates new grid
         self.state = state if state else self.generate_grid()
         self.directions = DIRECTIONS
 
+    # Generates empty grid
     @staticmethod
     def generate_grid():
         return [[None] * 4 for _ in [0, 1, 2, 3]]
 
+    # Returns list of all cell values
     def get_cell_values(self):
         values = []
 
@@ -41,21 +47,26 @@ class GameBoard:
 
         return values
 
+    # Returns the indices of all empty cells
     def get_empty_cells(self):
         return [[x, y] for y in [0, 1, 2, 3] for x in [0, 1, 2, 3] if not self.get_value_at_position(x, y)]
 
+    # Returns the number of empty cells
     def get_number_of_empty_cells(self):
         return len(self.get_empty_cells())
 
+    # Returns cell at position [x, y]
     def get_cell_at_position(self, x, y):
         if 0 <= x <= 3 and 0 <= y <= 3:
             return self.state[x][y]
         else:
             return None
 
+    # Returns cell at index
     def get_cell_at_index(self, index):
         return self.get_cell_at_position(index[0], index[1])
 
+    # Returns value at position [x, y]
     def get_value_at_position(self, x, y):
         cell = self.get_cell_at_position(x, y)
 
@@ -64,9 +75,11 @@ class GameBoard:
 
         return 0
 
+    # Returns value at index
     def get_value_at_index(self, index):
         return self.get_value_at_position(index[0], index[1])
 
+    # Resets merged value for all cells
     def prepare_cells_for_move(self):
         for x in [0, 1, 2, 3]:
             for y in [0, 1, 2, 3]:
@@ -75,32 +88,40 @@ class GameBoard:
                 if cell:
                     cell.merged = False
 
+    # Returns next spawning value
     @staticmethod
     def get_next_spawning():
         return random.choice([1, 2], p=[0.9, 0.1])
 
+    # Returns closes neighbour of cell at index curr in direction step
     def get_closest_neighbour(self, curr, step):
         prev = curr
         curr = list(add(curr, step))
 
+        # Iterates as long as curr is inside board and empty
         while 0 <= curr[0] <= 3 and 0 <= curr[1] <= 3 and not self.get_value_at_index(curr):
-            prev = curr
-            curr = list(add(curr, step))
+            prev = curr  # Sets prev to be curr
+            curr = list(add(curr, step))  # Makes one step in moving direction
 
+        # Returns closest neighbour as well as cell next to it
         return [prev, curr]
 
+    # Removes a cell from the game board
     def remove_cell_from_grid(self, cell):
         self.state[cell.position[0]][cell.position[1]] = None
 
+    # Adds a cell to the game board
     def add_cell_to_grid(self, cell):
         self.state[cell.position[0]][cell.position[1]] = cell
 
+    # Moves a cell
     def move_cell(self, cell, position):
         self.state[cell.position[0]][cell.position[1]] = None
         self.state[position[0]][position[1]] = cell
 
         cell.change_position(position)
 
+    # Makes a computer move. Returns whether game is over or not
     def make_computer_move(self):
         empty_cells = self.get_empty_cells()
         next_cell_index = random.choice(range(len(empty_cells))) if empty_cells else None
@@ -112,61 +133,76 @@ class GameBoard:
 
         return False
 
+    # Makes a player move. Returns whether move changed board or not
     def make_player_move(self, direction):
         does_move_change_state = False
-        direction = self.directions[direction]
+        direction = self.directions[direction]  # Direction to make move
 
         steps = direction[1]
 
+        # Resets whether cells are merged or not
         self.prepare_cells_for_move()
 
+        # Iterates through all cells
         for x in steps[0]:
             for y in steps[1]:
-                cell = self.get_cell_at_position(x, y)
+                cell = self.get_cell_at_position(x, y)  # Fetches cell at position
                 cell_index = [x, y]
 
+                # If cell is not empty
                 if cell:
+                    # Finds closest neighbour
                     closest_neighbour_indices = self.get_closest_neighbour(cell.position, direction[0])
                     closest_neighbour = self.get_cell_at_index(closest_neighbour_indices[1])
 
+                    # Merges same values
                     if closest_neighbour and closest_neighbour.value == cell.value and not \
                             closest_neighbour.merged:
                         merged_cell = Cell(closest_neighbour_indices[1], cell.value + 1, True)
 
+                        # Adds merged cell and removes old cell
                         self.add_cell_to_grid(merged_cell)
                         self.remove_cell_from_grid(cell)
 
                         cell.change_position(closest_neighbour_indices[1])
 
+                        # Tells that move is changed
                         does_move_change_state = True
-                    # Move
+                    # Moves cell
                     else:
                         self.move_cell(cell, closest_neighbour_indices[0])
 
-                    if cell.position != cell_index:
-                        does_move_change_state = True
+                        # Checks whether cell actually moved or not
+                        if cell.position != cell_index:
+                            does_move_change_state = True
 
+        # Returns whether move changed game board or not
         return does_move_change_state
 
+    # Evaluates game board
     def evaluate(self):
         return self.tidy()
 
+    # Returns maximum value on the game board
     def get_max_value(self):
         return max(max([self.get_value_at_position(x, y) for y in [0, 1, 2, 3]]) for x in [0, 1, 2, 3])
 
+    # Returns score of the game board
     def tidy(self):
         return sum((self.get_value_at_index(PATH[n]) ** 2) * (R ** n) for n in range(16))
 
+    # Returns clone of game board
     def clone(self):
         return GameBoard(deepcopy(self.state))
 
 
 class Cell:
     def __init__(self, position, value, merged=False):
-        self.position = position
-        self.value = value
+        self.position = position  # Cell position
+        self.value = value  # Cell value
 
-        self.merged = merged
+        self.merged = merged  # Whether cell is merged or not
 
+    # Change position of cell
     def change_position(self, position):
         self.position = position[:]
--- src/modules/module_four/twenty_forty_eight.py
+++ src/modules/module_four/twenty_forty_eight.py
@@ -3,6 +3,7 @@ from numpy import mean
 from modules.module_four.game_board import GameBoard, Cell
 
 
+# Probabilities for spawning values
 PROBABILITIES = {
     1: 0.9,
     2: 0.1
@@ -11,9 +12,13 @@ PROBABILITIES = {
 
 class TwentyFortyEight:
     def __init__(self, game_board=None, ui=None):
+        # Callback function for updating UI
         self.ui = ui
+
+        # Creates a new game board or sets game board to a supplied value
         self.game_board = GameBoard() if not game_board else game_board
 
+    # Returns what depth to use in the expectimax function
     def get_depth(self):
         number_of_empty_cells = self.game_board.get_number_of_empty_cells()
 
@@ -24,82 +29,113 @@ class TwentyFortyEight:
         else:
             return 3
 
+    # Finds best computer move using expectimax function
     def get_next_player_move(self):
         return self.evaluate_player_move(self.get_depth())['direction']
 
+    # Makes computer move, updates UI and returns whether move was legal or not
     def make_player_move(self):
-        next_move = self.get_next_player_move()
+        next_move = self.get_next_player_move()  # Finds next move using expectimax function
 
+        # Returns False if no move found
         if next_move is None:
             return False
 
+        # Makes the actual move
         self.game_board.make_player_move(next_move)
 
+        # Updates UI, if any
         if self.ui:
             self.ui.update_ui(self.game_board.state)
 
+        # Returns that the move was legal
         return True
 
+    # Makes computer move
     def make_computer_move(self):
+        # Makes move
         moved = self.game_board.make_computer_move()
 
+        # Updates UI if move was made
         if moved and self.ui:
             self.ui.update_ui(self.game_board.state)
 
+        # Returns whether move was made or not
         return moved
 
+    # Evaluates current game board, returning move expected to yield highest score
     def evaluate_player_move(self, depth):
         best_move = None
         best_score = -1
 
+        # Iterates through all possible moves
         for direction in self.game_board.directions:
+            # Clones game board
             game_board = self.game_board.clone()
 
+            # Checks whether move is legal or not
             if game_board.make_player_move(direction):
                 if depth == 0:
+                    # Calculates board score if depth is 0
                     result = {
                         'direction': direction,
                         'score': game_board.evaluate()
                     }
                 else:
+                    # Calculates expected board score if depth is > 0
                     result = TwentyFortyEight(game_board=game_board).evaluate_computer_move(depth)
 
+                # Compares calculated score to best score
                 if result['score'] > best_score:
                     best_score = result['score']
                     best_move = direction
 
+        # Returns best move and expected score
         return {
             'direction': best_move,
             'score': best_score
         }
 
+    # Evaluates current game board, returning weighted mean of computer moves
     def evaluate_computer_move(self, depth):
+        # All empty cells
         empty_cells = self.game_board.get_empty_cells()
 
         scores = []
 
+        # Iterates through all empty cells
         for empty_cell in empty_cells:
+            # Puts a 1 and 2 in each empty cell
             for value in [1, 2]:
                 cell = Cell(empty_cell, value)
 
+                # Clones game board and adds value to empty cell
                 game_board = self.game_board.clone()
                 game_board.add_cell_to_grid(cell)
 
+                # Calculates expected score of current board
                 result = TwentyFortyEight(game_board=game_board).evaluate_player_move(depth-1)
 
+                # Appends weighted expected score to list of possible computer moves
                 scores.append(result['score'] * PROBABILITIES[value])
 
+        # Returns weighted mean of all possible computer moves
         return {
             'direction': None,
             'score': mean(scores)
         }
 
+    # Runs the 2048 solver
     def run(self):
         is_game_over = not self.make_computer_move()
 
+        # Makes moves as long as the game isn't lost yet
         while not is_game_over:
+            # Makes the move
             self.make_player_move()
 
+            # Spawns new value
             is_game_over = not self.make_computer_move()
 
+        # Returns final score
         return 2 ** self.game_board.get_max_value()
